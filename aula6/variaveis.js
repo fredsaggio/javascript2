70074592e915ec7b7371e09da90f240adf50a82d
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-03 23:59:22 -0300

    att
    att

---
+++ aula6/variaveis.js
@@ -0,0 +1,5 @@
+// Sempre use LET ao invés de VAR. 
+
+// Use a estrutura camelCase.
+
+// Utilize as regras padrão para criar variáveis. 
---
+++ aula7/index.js
@@ -0,0 +1,8 @@
+// Aula sobre Constantes.
+
+//
+
+const teste = '1' 
+const teste2 = 2
+
+console.log(Number(teste) + teste2) 
