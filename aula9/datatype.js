b40c80a9e74a7eaced09ccfda3ad598267858844
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-05 12:11:50 -0300

    aula9
    att

---
+++ aula9/datatype.js
@@ -0,0 +1,10 @@
+ // String, Number, Undefined, Null, Boolean and Symbol
+
+ const nome = 'Fred'
+ const numero = 10 
+ const numeroFlutuante = 10.51
+ const teste = true 
+ const asasasa = null
+ let vassoura; 
+
+ console.log(typeof asasasa)
