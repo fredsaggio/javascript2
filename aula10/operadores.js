59f5cbe49526db49ccb89aa330f3e6a0f1da93b1
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-05 13:50:07 -0300

    att
    att

---
+++ aula10/operadores.js
@@ -0,0 +1,16 @@
+// Operadores e precedências
+
+// Aritméticos:
+
+// + - * / % **
+
+let num1 = 3
+let num2 = 2 
+let num5 = 4
+let num10 = 8
+
+/* console.log(num1 + num5 ** num2 % num10) */
+
+for(let c = 1; c <= num10; c++) {
+    console.log(`O valor atual é ${c}`)
+}
---
+++ aula10/operadores2.js
@@ -0,0 +1,6 @@
+// Incremento e Decremento
+
+
+const passo = 10;
+
+for(let cont = 0; cont <= 100; cont + passo) {}
