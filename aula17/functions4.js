15bbf303b2fe5f79f2b98dd1ce874cdcc09ef759
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-18 21:29:52 -0300

    att

---
+++ aula17/functions4.js
@@ -0,0 +1,14 @@
+const teste = function (n) {
+    return n ** 0.5
+}
+
+console.log(teste(16).toFixed(2))
+
+// ------------------------------------
+
+// Arrow function:
+
+const teste2 = n => n ** 2
+
+
+console.log(teste2(10).toFixed(2))
