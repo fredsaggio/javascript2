59ef7fd6c08bd9f3f4f28c631b6111f78c59c642
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-22 23:12:56 -0300

    att
    att

---
+++ aula21/operadores.js
@@ -0,0 +1,20 @@
+/* Operadores de comparação:
+
+> Maior que 
+< Menor que
+>= Maior igual à que
+<= Menor igual à que
+=== Igual estrito
+!== Diferente estrito
+
+
+Atenção: Não seu usa nem '==' e nem '!='. São sinais não usados na programação por conflitarem o código. 
+
+*/
+
+const num1 = 1 
+const num2 = '1'
+
+const teste = num1 !== num2
+
+console.log(teste)
