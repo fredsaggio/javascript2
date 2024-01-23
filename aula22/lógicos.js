cebbb4d1a98c7bc72777d563e43f9d743f5ff1dd
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-01-23 12:32:12 -0300

    att
    att

---
+++ aula22/lógicos.js
@@ -0,0 +1,14 @@
+/* 
+Operadores lógicos: 
+
+    && -> and -> e 
+    || -> or -> ou
+    ! -> not -> não
+
+*/
+
+console.log(true && true && true && true); // Deu true porque todos são 'true', caso um fosse false, daria 'false'.
+
+console.log(false || false); // Deu false porque os dois são 'false', se um fosse true, daria 'true'.
+
+console.log(!false); // Deu true por conta da negação utilizada antes do boolean. Sempre vai dar o valor contrário. 
