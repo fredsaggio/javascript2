0a9ad7483110fc4e41d422c9e23156435b0fdc97
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-02-01 22:39:34 -0300

    att
    att

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,20 +1,20 @@
 function allScope() {
 
-const altura = document.querySelector('input#input1')
-const peso = document.querySelector('input#input2')
-const resultado = document.querySelector('div#container-resultado')
+    const altura = document.querySelector('input#input1')
+    const peso = document.querySelector('input#input2')
+    const resultado = document.querySelector('div#container-resultado')
 
+}
 
 function enviar() {
+
+    const altura = document.querySelector('input#input1')
+    const peso = document.querySelector('input#input2')
+    const resultado = document.querySelector('div#container-resultado')
+
     if (altura.value.length === 0 || peso.value.length === 0) {
-        resultado.style.backgroundcolor = 'red'
+        resultado.style.backgroundColor = 'red'
         resultado.innerHTML = 'Resultado inválido!'
     }
 }
 
-
-
-
-
-
-}
