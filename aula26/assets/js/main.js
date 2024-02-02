9ac95316a6164a0aaec2b9abfa706d64af12099e
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-02-01 22:24:52 -0300

    att
    att

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,10 +1,16 @@
 function allScope() {
 
+const altura = document.querySelector('input#input1')
+const peso = document.querySelector('input#input2')
+const resultado = document.querySelector('div#container-resultado')
 
 
-
-
-
+function enviar() {
+    if (altura.value.length === 0 || peso.value.length === 0) {
+        resultado.style.backgroundcolor = 'red'
+        resultado.innerHTML = 'Resultado inválido!'
+    }
+}
 
 
 
