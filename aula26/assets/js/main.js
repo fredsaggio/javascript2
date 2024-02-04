613d8ff790471f56f8591f4d521ffe749b046cef
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-02-04 17:46:00 -0300

    att
    att

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,19 +1,28 @@
+const altura = document.querySelector('input#input1');
+const peso = document.querySelector('input#input2');
+const resultado = document.querySelector('div#container-resultado');
+
+
 function enviar() {
 
-    const altura = document.querySelector('input#input1')
-    const peso = document.querySelector('input#input2')
-    const resultado = document.querySelector('div#container-resultado')
+    if (altura.value.length === 0 && peso.value.length === 0) {
 
-    if (altura.value.length === 0 || altura.value.typeof !== Number && peso.value.length === 0 || peso.value.typeof !== Number){
         resultado.style.backgroundColor = 'salmon'
-        resultado.innerHTML = '<p>Peso e altura inválidos</p>'
+        resultado.innerHTML = '<p>Peso e altura inválidos</p>'  
 
-    } else if (altura.value.length === 0 || altura.value.typeof !== Number) {
-        resultado.style.backgroundColor = 'salmon'
+    } else if (altura.value.length === 0) {
+
+        resultado.style.backgroundColor = 'lightblue'
         resultado.innerHTML = '<p>Altura inválida</p>'
-    } else if (peso.value.length === 0 || peso.value.typeof !== Number) {
-        resultado.style.backgroundColor = 'salmon' 
+
+    } else if (peso.value.length === 0) {
+
+        resultado.style.backgroundColor = 'lightblue'
         resultado.innerHTML = '<p>Peso inválido</p>'
+
+    } else {
+        const imc = Number(altura.value) / Number(peso.value) ** Number(peso.value)
+        
     }
 
 }
---
+++ teste.js
@@ -0,0 +1,7 @@
+const teste = 1
+
+if (typeof teste === 'number') {
+    console.log('É um número')
+} else {
+    console.log('Não é um número')
+}
