bd6a75ca9da41bbc5dbc4f5c8253493dc25936a0
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-02-03 23:37:13 -0300

    ATT
    ATT

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,20 +1,19 @@
-function allScope() {
-
-    const altura = document.querySelector('input#input1')
-    const peso = document.querySelector('input#input2')
-    const resultado = document.querySelector('div#container-resultado')
-
-}
-
 function enviar() {
 
     const altura = document.querySelector('input#input1')
     const peso = document.querySelector('input#input2')
     const resultado = document.querySelector('div#container-resultado')
 
-    if (altura.value.length === 0 || peso.value.length === 0) {
-        resultado.style.backgroundColor = 'red'
-        resultado.innerHTML = 'Resultado inválido!'
-    }
-}
+    if (altura.value.length === 0 || altura.value.typeof !== Number && peso.value.length === 0 || peso.value.typeof !== Number){
+        resultado.style.backgroundColor = 'salmon'
+        resultado.innerHTML = '<p>Peso e altura inválidos</p>'
 
+    } else if (altura.value.length === 0 || altura.value.typeof !== Number) {
+        resultado.style.backgroundColor = 'salmon'
+        resultado.innerHTML = '<p>Altura inválida</p>'
+    } else if (peso.value.length === 0 || peso.value.typeof !== Number) {
+        resultado.style.backgroundColor = 'salmon' 
+        resultado.innerHTML = '<p>Peso inválido</p>'
+    }
+
+}
