c70e97ed9283478c50a6c30599a1c65b04517dad
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-03-14 22:55:38 -0300

    att
    att

--- aula27/ternarios.js
+++ aula27/ternarios.js
@@ -1,2 +1,8 @@
 // Operadores ternários: "?" seguido de duas afirmativas, uma true e uma false, divididas por um ":".
 
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000? 'Usuário VIP' : 'Usuário normal';
+let corUsuario = null;
+const corPadrao = corUsuario || 'Preto' // Caso o usuário não selecione uma cor padrão, ela será preto, como mostra o código, caso ele queira uma cor diferente, terá que escolher e essa cor será armazenada na variável 'corUsuario'.
+
+console.log(nivelUsuario, corPadrao) 
