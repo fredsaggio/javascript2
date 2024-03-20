3c554375be63a767dd08c40a258961f41fde0da4
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-03-20 20:11:10 -0300

    escopo com let e var

---
+++ aula31/letvar.js
@@ -0,0 +1,32 @@
+let nome = 'Fred';
+
+let verdadeiro = true;
+
+if (verdadeiro) {
+    let nome = 'Letícia';
+    console.log(nome);
+    if (verdadeiro) {
+        let nome = 'Sofia';
+        console.log(nome);
+    }
+}
+
+var teste = 'Ronaldo'
+
+function oiMano(){
+    var teste = 'patrícia';
+    console.log(teste)
+    
+    if (verdadeiro) {
+        var teste = 'peixe'
+        console.log(teste)
+    }
+}
+
+oiMano();
+
+console.log(teste)
+
+
+
+
