b6109c4b49d9d3ac978dedbd43cc1494788cc200
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-04-02 20:34:52 -0300

    att
    att

---
+++ aula37/forOf.js
@@ -0,0 +1,7 @@
+const frutas = ['ameixa', 'jabuticaba', 'amora'];
+
+for(let i of frutas){ // Esse "for" faz a variável escolhida passar entre os valores dos índices do Array.
+    console.log(i)
+}
+
+
